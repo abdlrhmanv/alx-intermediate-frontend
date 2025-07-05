0356bac9cf1e292b1be33d7076d62cd4ee777bf5
Abdlrhman <abdlrhmanv@icloud.com>
2025-07-05 17:47:43 +0300

    1. Creating a Responsive CSS Grid Layout with Tailwind CSS

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./src/*.html"
+  ],
+  safelist: [
+    'bg-blue-200',
+    'bg-blue-300', 
+    'bg-blue-400',
+    'p-4',
+    'gap-4'
+  ],
   theme: {
     extend: {},
   },
